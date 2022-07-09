c2b8fd69cde1deaabc27bd52dfd9ceb9fda4a3c5
ken <redcool007@qq.com>
2022-07-09 16:50:21 +0800

    update PowerGradient unity 2021

--- PowerGradient/TestGradient.cs
+++ PowerGradient/TestGradient.cs
@@ -1,32 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
-
-public class TestGradient : MonoBehaviour
+﻿namespace PowerUtilities
 {
-    public PowerGradient g1;
-    public PowerGradient g2;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEditor;
+    using UnityEngine;
 
-    public Gradient g;
-
-    public float test;
-
-    [Range(0,1)]public float rate;
-    public float speed =1f;
-
-    // Start is called before the first frame update
-    void Start()
+    public class TestGradient : MonoBehaviour
     {
-        
-    }
+        public PowerGradient g1;
+        public PowerGradient g2;
 
-    // Update is called once per frame
-    void Update()
-    {
-        rate += speed * Time.deltaTime;
+        public Gradient g;
 
-        var c = g1.Evaluate(rate);
-        GetComponent<MeshRenderer>().sharedMaterial.color = c;
+        public float test;
+
+        [Range(0, 1)] public float rate;
+        public float speed = 1f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rate += speed * Time.deltaTime;
+
+            var c = g1.Evaluate(rate);
+            GetComponent<MeshRenderer>().sharedMaterial.color = c;
+        }
     }
 }
